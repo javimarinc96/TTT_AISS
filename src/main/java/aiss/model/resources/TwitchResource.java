424a0e73b6deadc1d6f8bdb3c520c88f61d7af99
Lord <lordkelvin2099@gmail.com>
2019-05-20 16:09:29 +0200

    TwitchResource arreglado

--- src/main/java/aiss/model/resources/TwitchResource.java
+++ src/main/java/aiss/model/resources/TwitchResource.java
@@ -11,29 +11,40 @@ import org.restlet.resource.ClientResource;
 import aiss.model.twitch.Streams;
 
 public class TwitchResource {
+	
 	private static final Logger log = Logger.getLogger(TwitchResource.class.getName());
+	private final String access_token;
+    private final String uri = "https://api.twitch.tv/helix/streams";
+	
+	
+    public TwitchResource(String access_token) {
+        this.access_token = access_token;
+    }
 	
 	public Streams getStreams(String user_id) throws UnsupportedEncodingException {
 		//Asegurarnos que la cadena esta en el formato correcto
 		
 		String user_id2 = URLEncoder.encode(user_id, "UTF-8");
+		
+		log.log(Level.FINE, "user_id: ", user_id2);
+		log.log(Level.FINE, "token: ", this.access_token);
 				
 		// Escribir la url de busqueda
 		
-		String uri = "https://api.twitch.tv/helix/streams?user_id="+user_id2;
+		String url = uri + "user_id=" + user_id2;
 		
 		//Añadimos un log
 		
-		log.log(Level.FINE, "Search for streams: ", uri);
+		log.log(Level.FINE, "Search for streams: ", url);
 		
 		//Pedir al servicio restful el recurso que queremos (lo devuelve en json)
 		
-		ClientResource cr = new ClientResource(uri);
+		ClientResource cr = new ClientResource(url);
 		
 		//Convertir ese recurso en formato java
 		
-		Streams TwitchSearch= cr.get(Streams.class);
+		Streams twitchSearch= cr.get(Streams.class);
 		
-	    return TwitchSearch;
+	    return twitchSearch;
 	}
 }
